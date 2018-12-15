92db26c6f8bc3d4dbe459e06e4700c203e3a596e
ozcanat <ozcanat@itu.edu.tr>
2018-12-16 02:23:09 +0300

    deleted .idea

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/atakanozcan/PycharmProjects/irem_django2.1_aynisi/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
