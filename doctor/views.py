c1a7cd2d71a69f6ed12da5d33e57561bb24e53ad
Merve Elif Demirtaş <demirtasm16@itu.edu.tr>
2018-12-16 18:31:28 +0300

    some modifications were done

--- doctor/views.py
+++ doctor/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, render_to_response
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.models import User
+from accounts.models import UserProfile
 
 # Create your views here.
 
 
 @login_required
+@user_passes_test(lambda u: UserProfile.objects.get(staff="Doctor") in u.groups.all())
 def doctor_home(request):
     return render(request, 'doctor/doctor_home.html', locals())
